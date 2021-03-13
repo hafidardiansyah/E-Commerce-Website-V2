dedd12cb2aeefbd79b86868092eb711c8894c69c
Hafid Ardiansyah <62227964+hafidardiansyah@users.noreply.github.com>
2021-03-13 13:01:23 +0700

    update

--- README.md
+++ README.md
@@ -1,90 +1,42 @@
-<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
+<h1 align="center">Selamat datang di ITShop! 👋</h1>
 
-<p align="center">
-<a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/dt/laravel/framework" alt="Total Downloads"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/v/laravel/framework" alt="Latest Stable Version"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/l/laravel/framework" alt="License"></a>
+![ITShop - Screenshot](/images/home.png)
+
+<p align ="Center">
+
+<img src="https://img.shields.io/github/issues/hafidardiansyah/ITShop?style=flat-square">
+<img src="https://img.shields.io/github/stars/hafidardiansyah/ITShop?style=flat-square">
+<img src="https://img.shields.io/github/forks/hafidardiansyah/ITShop?style=flat-square">
+
 </p>
 
-## Fitur
+### 🤔 Apa itu ITShop?
 
--   [x] Role
--   [x] Login
--   [x] Register
--   [x] Search
--   [x] Product
--   [x] Category
--   [x] Purchase
--   [x] Cart
--   [x] Order
--   [x] Payment
+ITShop adalah website jual beli online berkaitan tentang IT, yang dibuat oleh <a href="https://github.com/hafidardiansyah"> Hafid Ardiansyah</a>. Menggunakan Framework Bootstrap 4.5 & Laravel 8.
 
-## Screenshot Project
+### 🎉 Kenapa membuat ITShop?
 
-<table>
-    <tr>
-        <td><img src="images/home.png"></td>
-        <td><img src="images/login.png"></td>
-        <td><img src="images/new.png"></td>
-    </tr>
-    <tr>
-        <td><img src="images/cart.png"></td>
-        <td><img src="images/order.png"></td>
-        <td><img src="images/purchase.png"></td>
-    </tr>
-</table>
+Website ini saya buat untuk melatih saya menggunakan Laravel dan Bootstrap.
 
-## About Laravel
+### :star: Fitur
 
-Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
+-   CRUD (Create Read Update Delete)
+-   Autentikasi (Login, Register & Logout)
+-   2 Role (Admin & Pembeli)
+-   Pencarian
+-   Pagination
+-   Responsive website
 
--   [Simple, fast routing engine](https://laravel.com/docs/routing).
--   [Powerful dependency injection container](https://laravel.com/docs/container).
--   Multiple back-ends for [session](https://laravel.com/docs/session) and [cache](https://laravel.com/docs/cache) storage.
--   Expressive, intuitive [database ORM](https://laravel.com/docs/eloquent).
--   Database agnostic [schema migrations](https://laravel.com/docs/migrations).
--   [Robust background job processing](https://laravel.com/docs/queues).
--   [Real-time event broadcasting](https://laravel.com/docs/broadcasting).
+### :link: Social Media
 
-Laravel is accessible, powerful, and provides tools required for large, robust applications.
+-   Instagram : <a href="http://instagram.com/dev.hafid"> @dev.hafid</a>
+-   Dribbble : <a href="https://dribbble.com/hafidardiansyah"> hafidardiansyah</a>
 
-## Learning Laravel
+### 📝 Lisensi
 
-Laravel has the most extensive and thorough [documentation](https://laravel.com/docs) and video tutorial library of all modern web application frameworks, making it a breeze to get started with the framework.
+-   Copyright © 2020 Hafid Ardiansyah.
+-   **ITShop MIT License.**
 
-If you don't feel like reading, [Laracasts](https://laracasts.com) can help. Laracasts contains over 1500 video tutorials on a range of topics including Laravel, modern PHP, unit testing, and JavaScript. Boost your skills by digging into our comprehensive video library.
+---
 
-## Laravel Sponsors
-
-We would like to extend our thanks to the following sponsors for funding Laravel development. If you are interested in becoming a sponsor, please visit the Laravel [Patreon page](https://patreon.com/taylorotwell).
-
-### Premium Partners
-
--   **[Vehikl](https://vehikl.com/)**
--   **[Tighten Co.](https://tighten.co)**
--   **[Kirschbaum Development Group](https://kirschbaumdevelopment.com)**
--   **[64 Robots](https://64robots.com)**
--   **[Cubet Techno Labs](https://cubettech.com)**
--   **[Cyber-Duck](https://cyber-duck.co.uk)**
--   **[Many](https://www.many.co.uk)**
--   **[Webdock, Fast VPS Hosting](https://www.webdock.io/en)**
--   **[DevSquad](https://devsquad.com)**
--   **[Curotec](https://www.curotec.com/)**
--   **[OP.GG](https://op.gg)**
-
-## Contributing
-
-Thank you for considering contributing to the Laravel framework! The contribution guide can be found in the [Laravel documentation](https://laravel.com/docs/contributions).
-
-## Code of Conduct
-
-In order to ensure that the Laravel community is welcoming to all, please review and abide by the [Code of Conduct](https://laravel.com/docs/contributions#code-of-conduct).
-
-## Security Vulnerabilities
-
-If you discover a security vulnerability within Laravel, please send an e-mail to Taylor Otwell via [taylor@laravel.com](mailto:taylor@laravel.com). All security vulnerabilities will be promptly addressed.
-
-## License
-
-The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+-   **Made with ❤️ by Hafid Ardiansyah**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,12 +31,10 @@
             <div class="container">
 
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    ITShop
                 </a>
 
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -44,8 +42,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                            <a href="{{ route('products') }}"
-                                class="nav-link{{ request()->segment(1) == 'products' ? ' active' : '' }}">
+                            <a href="{{ route('products') }}" class="nav-link{{ request()->segment(1) == 'products' ? ' active' : '' }}">
                                 List Product
                             </a>
                         </li>
@@ -53,8 +50,7 @@
 
                     <ul class="navbar-nav">
                         <form action="{{ route('search') }}" method="GET" class="form-inline my-2 my-lg-0">
-                            <input class="form-control mr-sm-2" type="text" placeholder="Enter keyword..."
-                                aria-label="Search" name="keyword" autocomplete="on">
+                            <input class="form-control mr-sm-2" type="text" placeholder="Enter keyword..." aria-label="Search" name="keyword" autocomplete="on">
                             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
                                 Search
                             </button>
@@ -65,55 +61,49 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link{{ request()->segment(1) == 'login' ? ' active' : '' }}"
-                                        href="{{ route('login') }}">{{ __('Login') }}
-                                    </a>
-                                </li>
-                            @endif
+                        @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link{{ request()->segment(1) == 'login' ? ' active' : '' }}" href="{{ route('login') }}">{{ __('Login') }}
+                            </a>
+                        </li>
+                        @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link{{ request()->segment(1) == 'register' ? ' active' : '' }}"
-                                        href="{{ route('register') }}">{{ __('Register') }}
-                                    </a>
-                                </li>
-                            @endif
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link{{ request()->segment(1) == 'register' ? ' active' : '' }}" href="{{ route('register') }}">{{ __('Register') }}
+                            </a>
+                        </li>
+                        @endif
                         @else
 
-                            @if (Auth::user()->role == 1)
-                                <a href="{{ route('cart') }}"
-                                    class="nav-link{{ request()->segment(1) == 'cart' ? ' active' : '' }}"><i
-                                        class='bx bxs-cart'></i>
+                        @if (Auth::user()->role == 1)
+                        <a href="{{ route('cart') }}" class="nav-link{{ request()->segment(1) == 'cart' ? ' active' : '' }}"><i class='bx bxs-cart'></i>
+                        </a>
+                        @endif
+
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                @if (Auth::user()->role == 0)
+                                <a class="dropdown-item" href="{{ route('create') }}">New Product</a>
+                                <a class="dropdown-item" href="{{ route('payment') }}">Payment</a>
+                                <a class="dropdown-item" href="{{ route('purchase') }}">Purchase</a>
+                                @endif
+                                @if (Auth::user()->role == 1)
+                                <a class="dropdown-item" href="{{ route('my-order') }}">My Order</a>
+                                @endif
+
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}
                                 </a>
-                            @endif
 
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    @if (Auth::user()->role == 0)
-                                        <a class="dropdown-item" href="{{ route('create') }}">New Product</a>
-                                        <a class="dropdown-item" href="{{ route('payment') }}">Payment</a>
-                                        <a class="dropdown-item" href="{{ route('purchase') }}">Purchase</a>
-                                    @endif
-                                    @if (Auth::user()->role == 1)
-                                        <a class="dropdown-item" href="{{ route('my-order') }}">My Order</a>
-                                    @endif
-
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
@@ -145,7 +135,6 @@
             }
 
         }
-
     </script>
 </body>
 
